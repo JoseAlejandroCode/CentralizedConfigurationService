1ac61496cd12e47a2acbb1a4086c997c9567efda
Jose Alejandro <paredes_191@hotmail.com>
2019-11-26 21:41:16 -0500

    add eureka client

--- src/main/java/com/microservice/centralizedconfigurationservice/CentralizedConfigurationServiceApplication.java
+++ src/main/java/com/microservice/centralizedconfigurationservice/CentralizedConfigurationServiceApplication.java
@@ -2,8 +2,12 @@ package com.microservice.centralizedconfigurationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableCircuitBreaker
+@EnableEurekaClient
 @EnableConfigServer
 @SpringBootApplication
 public class CentralizedConfigurationServiceApplication {
